a3931995601de7f7dce88281303ff5f85f17a006
JosueRFP <josue.petri.senac@gmail.com>
2025-07-18 11:31:03 -0300

    Mais animações

--- Boss 3D/Assets/Scripts/BossBehaviour.cs
+++ Boss 3D/Assets/Scripts/BossBehaviour.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public enum BossAtacks 
@@ -8,14 +9,21 @@ public enum BossAtacks
 
 public class BossBehaviour : MonoBehaviour
 {
+    [SerializeField] GameObject spear;
+    [SerializeField] Transform spearPos;
     [SerializeField] float life = 100;
     [SerializeField] float maxLife;
-     
+    [SerializeField] Transform player;
+    float range;
+    
+    Animator animator;
     BossAtacks atacks;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         maxLife = life;
+        animator = GetComponent<Animator>();
+        ChangeBehaviour(BossAtacks.Close);
     }
 
     // Update is called once per frame
@@ -25,5 +33,39 @@ public class BossBehaviour : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        if (distance > range)
+        {
+            StartCoroutine(ChangeBehaviour(BossAtacks.Renge));
+            ChangeBehaviour(BossAtacks.Renge);
+        }
+            
+
+        if (distance < range)
+            ChangeBehaviour(BossAtacks.Close);
+    }
+
+    IEnumerator ChangeBehaviour(BossAtacks behaviour)
+    {
+        switch (behaviour) 
+        {
+            case BossAtacks.Close:
+                animator.SetFloat("Atack", range);
+                break;
+            case BossAtacks.Area:
+                yield return null;
+                break;
+            case BossAtacks.Renge:
+                Instantiate(spear);
+                break;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+            Destroy(collision.gameObject);
     }
 }
